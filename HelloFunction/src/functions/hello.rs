cb4df116f79d6a1db5320ae2756e23f3230bc33c
Fabricio Sperafico Sonego - Promob <fabricio.sonego@promob.com>
2021-07-23 15:45:18 -0300

    removed wrong project

--- HelloFunction/src/functions/hello.rs
+++
@@ -1,9 +0,0 @@
-use azure_functions::{
-    bindings::{HttpRequest, HttpResponse},
-    func,
-};
-
-#[func]
-pub fn hello(req: HttpRequest) -> HttpResponse {
-    "Hello from Rust!".into()
-}
--- HelloFunction/src/main.rs
+++
@@ -1,5 +0,0 @@
-mod functions;
-
-fn main() {
-    azure_functions::worker_main(std::env::args(), functions::EXPORTS);
-}
