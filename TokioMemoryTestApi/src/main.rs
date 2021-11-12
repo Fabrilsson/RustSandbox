fe486cf00f8998029b4f0cdf4928207d3a571376
fabrilsson <fabriciosperafico@outlook.com>
2021-11-11 23:06:32 -0300

    memory test api

--- TokioMemoryTestApi/src/main.rs
+++ TokioMemoryTestApi/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    
+    let hello = warp::path!("bigstringRust")
+        .map(|| std::iter::repeat("x").take(10 * 1024).collect::<String>());
+
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
--- TokioMemoryTestApi/target/debug/TokioMemoryTestApi.d
+++ TokioMemoryTestApi/target/debug/TokioMemoryTestApi.d
@@ -1,1 +1,1 @@
-C:\Users\fabri\source\repo\RustSandbox\TokioMemoryTestApi\target\debug\TokioMemoryTestApi.exe: C:\Users\fabri\source\repo\RustSandbox\TokioMemoryTestApi\src\main.rs
+/home/fabrilsson/Documents/source/repo/RustSandbox/TokioMemoryTestApi/target/debug/TokioMemoryTestApi: /home/fabrilsson/Documents/source/repo/RustSandbox/TokioMemoryTestApi/src/main.rs
