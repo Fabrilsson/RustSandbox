3027eb0c783d7a532ec683fa4bd741cf7c23da9d
fabrilsson <fabriciosperafico@outlook.com>
2022-04-03 23:08:22 -0300

    .

--- TokioMemoryTestApi/src/main.rs
+++ TokioMemoryTestApi/src/main.rs
@@ -3,10 +3,17 @@ use warp::Filter;
 #[tokio::main]
 async fn main() {
     
-    let hello = warp::path!("bigstringRust")
+    static CONST_STRING: &'static str = "this is a static string";
+
+    let bigstring_rust = warp::path!("bigstringRust")
         .map(|| std::iter::repeat("x").take(10 * 1024).collect::<String>());
 
-    warp::serve(hello)
+    let bigstring_static_rust = warp::path!("bigstaticstringRust")
+    .map(|| CONST_STRING);
+
+    let routes = bigstring_rust.or(bigstring_static_rust);
+
+    warp::serve(routes)
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
